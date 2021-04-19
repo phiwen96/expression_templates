c683f0f6a2333de7e3dbcb433fd49247f221bc01
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-04-19 17:54:36 +0200

    a

---
+++ tests/include/test.hpp
@@ -0,0 +1,20 @@
+#include <catch2/catch.hpp>
+#include <expression_templates/expression.hpp>
+#include <expression_templates/type.hpp>
+#include <expression_templates/types.hpp>
+//using namespace std;
+/**
+ bool = types <int, char, string> | identical
+ 
+ */
+
+struct A{};
+
+auto run () -> int 
+{
+    constexpr A a;
+//    constexpr auto a0 = types <A, char, int> | ;
+    auto e0 = type <int> ;
+}
+
+
